4027666ce650624ccc404d0e4329d0ccdae9a11b
Md Nuruddin Osman <mdnuruddin378350@gmail.com>
2023-10-30 01:07:55 +0600

    finsweet

---
+++ js/script.js
@@ -0,0 +1,59 @@
+{/* <div class="we_for_bg">
+                        <div class="we_details">
+                            <h2>We want to serve the world around us</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do.</p>
+                            <button>Visit</button>
+                        </div>
+                        <div class="we_details_right">
+                            <div class="we_image">
+                                <img src="./images/quote_icon.png" alt="">
+                            </div>
+                        </div>
+                    </div> */}
+
+
+
+                    // .we_content{
+                    //     padding: 128px 366px;
+                    //     display: flex;
+                    //     justify-content: space-between;
+                    //     align-items: center;
+                    //     width: 968px;
+                    //     height: 480px;
+                    // }
+                    
+                    // .we_for_bg{
+                    //     background-color: #FFF;
+                    //     display: flex;
+                    //     align-items: center;
+                    //     column-gap: 117px;
+                    // }
+                    // .we_details{
+                    //     padding: 72px 0 56px 48px;
+                    // }
+                    
+                    
+                    // .we_details h2{
+                    //     width: 368px;
+                    //     color: #161722;
+                    //     font-family: Roboto Condensed;
+                    //     font-size: 48px;
+                    //     font-style: normal;
+                    //     font-weight: 700;
+                    //     line-height: 64px;
+                    //     letter-spacing: -0.48px;
+                    //     text-transform: uppercase;
+                    // }
+                    
+                    // .we_details p{
+                    //     width: 302px;
+                    //     color: #161722;
+                    //     font-family: Roboto Condensed;
+                    //     font-size: 16px;
+                    //     font-style: normal;
+                    //     font-weight: 400;
+                    //     line-height: 24px; 
+                    // }
+                    
+                    
+                                       
